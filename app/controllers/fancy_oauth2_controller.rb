3d53fdf46a546f4d5a97c13bb562705b41c10d8a
Vladimir Yartsev <vovayartsev@gmail.com>
2011-12-18 17:11:42 +0400

    error output

--- app/controllers/fancy_oauth2_controller.rb
+++ app/controllers/fancy_oauth2_controller.rb
@@ -3,16 +3,21 @@ class FancyOauth2Controller < ActionController::Base
   def callback
     code = params[:code]
     uri = FancyOauth2.callback_url_builder.call(request)
-    token = FancyOauth2.client.auth_code.get_token(code, :redirect_uri => uri)
-    if token.token
-      @auth = {
-        access_token: token.token, 
-        expires_in: token.expires_in, 
-        refresh_token: token.refresh_token
-      }
-      render 'callback_success', :layout => "fancy_oauth2_popup"
-    else
-      render 'callback_error', :layout => "fancy_oauth2_popup"
+    begin
+        token = FancyOauth2.client.auth_code.get_token(code, :redirect_uri => uri)
+        if token.token
+          @auth = {
+            access_token: token.token, 
+            expires_in: token.expires_in, 
+            refresh_token: token.refresh_token
+          }
+          render 'callback_success', :layout => "fancy_oauth2_popup"
+        else
+          render 'callback_error', :layout => "fancy_oauth2_popup"
+        end
+    rescue Exception => e
+        @error = e.message + "\nURI: #{uri}\nCODE: #{code}"
+        render 'callback_error', :layout => "fancy_oauth2_popup"
     end
   end
 
